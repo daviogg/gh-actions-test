f1d61c79685ec34dcdabbc00497e0f2f5e1e06ea
daviogg <daviogg@protonmail.com>
2023-04-20 15:48:42 +0200

    add firebase function

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  response.send(`Hello from Firebase!, environment ${process.env.ENV_NAME}`);
+});
